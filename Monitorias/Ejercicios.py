7ab6a1dc21517a5b1c2f18a1d5f3df115c3b1f7a
StivenRuiz2005 <carlosruizrojas2005@gmail.com>
2023-09-25 15:30:12 -0500

    Usando commit

--- Monitorias/Ejercicios.py
+++ Monitorias/Ejercicios.py
@@ -1,6 +1,7 @@
 cedula = int(input("Digite la cedula: "))
 salario_b = float(input("Ingrese su salario: "))
 age = int(input("Ingrese su año de vinculacion: "))
+ejemplo = "hola"
 
 def Salario(Cedula,Salario_b,Age):
     if (Salario_b > 1200000 and Age > 1995):
@@ -8,7 +9,7 @@ def Salario(Cedula,Salario_b,Age):
     elif (Salario_b < 550000 or Age == 1995):
         neto = Salario_b * 0.03
     else:
-        neto = Salario_b * 0.04
+        neto = Salario_b * 0.05
 
     salario_final = (Salario_b + neto) - (Salario_b * 0.04)
 
